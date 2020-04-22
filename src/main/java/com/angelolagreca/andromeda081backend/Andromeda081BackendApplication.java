ab0ce70342d4c2282bdef4c2f1118075ec64ab20
Angelo La Greca <anccelo@iol.it>
2020-04-22 17:23:18 +0200

    first commit

---
+++ src/main/java/com/angelolagreca/andromeda081backend/Andromeda081BackendApplication.java
@@ -0,0 +1,13 @@
+package com.angelolagreca.andromeda081backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Andromeda081BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Andromeda081BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angelolagreca/andromeda081backend/Andromeda081BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelolagreca.andromeda081backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Andromeda081BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
